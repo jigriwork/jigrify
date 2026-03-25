137e63a497fe8fa09a02e65e705422b96b5ccb69
jigriwork <jigriwork@gmail.com>
2026-03-25 18:54:09 +0530

    cleanup: add pwa baseline and final phase-1 progress update

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // baseline PWA SW; advanced caching strategies will be added in next phases
+});
